2f86e1ff83d131fecb9d1755979bda95a740750c
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2023-01-14 21:57:04 +0530

    added POST

--- app/app.py
+++ app/app.py
@@ -20,13 +20,15 @@ def home_endpoint():
     return 'hello'
 
 
-@app.route('/pred', methods=['GET'])
+@app.route('/pred', methods=['GET','POST'])
 def get_prediction():
-    if request.method == 'GET':
+    if (request.method == 'GET') or (request.method == 'POST'):
         jdata = request.get_json()
         qdf=pd.DataFrame(jdata)
         pred=model.predict(qdf)
         send="Survives" if pred == 1 else "Dies"
+    else:
+        send="Invalid"
     return jsonify(send)
 
 if __name__ == '__main__':
